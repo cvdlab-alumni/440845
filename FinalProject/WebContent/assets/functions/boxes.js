5b3398285afbfb4bb97f3aa0ddb63d3bdd4d33c3
margherita <margheritasidoti@outlook.com>
2014-09-24 16:45:26 +0200

    rimuovi duplicato

--- FinalProject/WebContent/assets/functions/boxes.js
+++
@@ -1,6 +0,0 @@
-function box(x,y,z){
-	var geometry = new THREE.BoxGeometry( x,y,z );
-	var material = new THREE.MeshBasicMaterial( {color: 0x00ff00, transparent: true, opacity: 0.5} );
-	var cube = new THREE.Mesh( geometry, material );
-	return cube;
-}
